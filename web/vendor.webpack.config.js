3f91c930370d0a12b03fbd5f66a349e9b5b37fab
pyros2097 <pyros2097@gmail.com>
2017-07-15 17:45:54 +0530

    add more libs

--- web/vendor.webpack.config.js
+++ web/vendor.webpack.config.js
@@ -16,6 +16,12 @@ const plugins = [
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     __DEV__,
   }),
+  // Moment.js is an extremely popular library that bundles large locale files
+  // by default due to how Webpack interprets its code. This is a practical
+  // solution that requires the user to opt into importing specific locales.
+  // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+  // You can remove this if you don't use Moment.js:
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 
   ...(__DEV__ ? [] : config.productionPlugins),
 
@@ -30,10 +36,16 @@ module.exports = {
     // Put react-native-web / react dependencies in here.
     'react': [
       'react-native-web',
+      'react-apollo',
+      'react-navigation',
     ],
     // Put any other other core libs in here. (immutable, redux, localforage, etc.)
-    // 'core': [
-    // ],
+    'core': [
+      'cuid',
+      'fetch-everywhere',
+      'lodash',
+      'moment',
+    ],
   },
   output: {
     filename: outputFilename,
